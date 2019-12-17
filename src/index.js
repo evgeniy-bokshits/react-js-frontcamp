f3ff806be63635280f7df5cffdd47b1524b89101
Bokshyts, Yauheni <yauheni.bokshits@thomsonreiters.com>
2019-12-18 01:52:08 +0300

    init app with webpack + babel

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+
+console.log("Init App =)");
+ReactDOM.render(<App />, document.getElementById("root"));
